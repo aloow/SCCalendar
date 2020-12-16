63670a873616d38b62f1125ee52636e7551a7407
shaoqing <xuesq@pvc123.com>
2020-12-16 18:07:03 +0800

    完善

---
+++ Pods/Target Support Files/Pods-SCCalendar/Pods-SCCalendar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SCCalendar : NSObject
+@end
+@implementation PodsDummy_Pods_SCCalendar
+@end
--- SCCalendar/ViewController.swift
+++ SCCalendar/ViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
+import FloatingPanel
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FloatingPanelControllerDelegate {
 
+    var fpc: FloatingPanelController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupFPC()
+        
+        
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        self.present(fpc, animated: true, completion: nil)
+    }
+    
+    func setupFPC() {
+        // Initialize a `FloatingPanelController` object.
+        fpc = FloatingPanelController()
+
+        fpc.delegate = self // Optional
+        fpc.layout = MyFloatingPanelLayout()
+        
+        // Set a content view controller.
+        let contentVC = ContentViewController()
+        fpc.set(contentViewController: contentVC)
+
+        // Track a scroll view(or the siblings) in the content view controller.
+//        fpc.track(scrollView: contentVC.tableView)
+
+        
+        
+        // Create a new appearance.
+        let appearance = SurfaceAppearance()
+
+        // Define shadows
+        let shadow = SurfaceAppearance.Shadow()
+        shadow.color = UIColor.black
+        shadow.offset = CGSize(width: 0, height: 16)
+        shadow.radius = 16
+        shadow.spread = 8
+        appearance.shadows = [shadow]
+
+        // Define corner radius and background color
+        appearance.cornerRadius = 18.0
+//        appearance.backgroundColor = .clear
+
+        // Set the new appearance
+        fpc.surfaceView.appearance = appearance
+        
+        
+        // Add and show the views managed by the `FloatingPanelController` object to self.view.
+        fpc.addPanel(toParent: self)
+    }
+    
+    
 
 }
 
+
+class MyFloatingPanelLayout: FloatingPanelLayout {
+    let position: FloatingPanelPosition = .bottom
+    let initialState: FloatingPanelState = .tip
+    var anchors: [FloatingPanelState: FloatingPanelLayoutAnchoring] {
+        return [
+            .half: FloatingPanelLayoutAnchor(fractionalInset: 0.5, edge: .bottom, referenceGuide: .safeArea),
+            .tip: FloatingPanelLayoutAnchor(absoluteInset: 44.0, edge: .bottom, referenceGuide: .safeArea),
+        ]
+    }
+}
